ddaf5612deaa0ce23dda4fa587ac73abdec28619
sayrekas <chaotichoice@gmail.com>
2024-05-12 09:24:38 +0300

    Створення та реєстрація нової моделі, яка розширює функціонал стандартної моделі User і додає в неї нове поле "player_tag" для подальшої роботи з ним в профілі користувача

--- clash_inspector/user_profile/models.py
+++ clash_inspector/user_profile/models.py
@@ -1,2 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+
+
+class UserProfile(User):
+    player_tag = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Профіль гравця'
+        verbose_name_plural = 'Профілі гравців'
