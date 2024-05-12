e62ad52dcad9f6a82c81f4a4ec251757fe50435b
sayrekas <chaotichoice@gmail.com>
2024-05-12 09:21:32 +0300

    Змінено маршрутизацію для входу та реєстрації акаунту, тепер вона не проходить через user, а має свій окремий маршрут

--- clash_inspector/app/urls.py
+++ clash_inspector/app/urls.py
@@ -3,9 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from user_profile.views import register_page, login_page, log_out
+
 urlpatterns = [
     path('admin/', admin.site.urls, ),
     path('', include('search.urls')),
-    path('profile/', include('user_profile.urls'))
+    path('profile/', include('user_profile.urls')),
+    path('register/', register_page, name='register_page'),
+    path('login/', login_page, name='login_page'),
+    path('log_out/', log_out, name='log_out')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
